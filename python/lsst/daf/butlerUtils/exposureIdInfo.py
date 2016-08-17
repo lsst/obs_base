f9cafd3088f5f7f4ab85d79a114d0f70f10318eb
Nate Pease <n8pease@gmail.com>
2016-08-17 09:30:03 -0700

    futurize

--- python/lsst/daf/butlerUtils/exposureIdInfo.py
+++ python/lsst/daf/butlerUtils/exposureIdInfo.py
@@ -19,6 +19,9 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
+
+from past.builtins import long
+
 __all__ = ["ExposureIdInfo"]
 
 
@@ -46,7 +49,7 @@ class ExposureIdInfo(object):
     that are not entirely clear (this is DM-6664).
     """
 
-    def __init__(self, expId=0L, expBits=1, maxBits=64):
+    def __init__(self, expId=0, expBits=1, maxBits=64):
         """!Construct an ExposureIdInfo
 
         See the class doc string for an explanation of the arguments.
