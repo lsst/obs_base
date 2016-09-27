eaa405e4e73468570909adbaca06aecfbb5a648e
Nate Pease <n8pease@gmail.com>
2016-09-27 12:53:45 -0700

    create composite test case

---
+++ python/lsst/obs/base/test/compositeMapper.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+#
+# LSST Data Management System
+# Copyright 2016 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+import lsst.daf.persistence as dafPersist
+from lsst.obs.base import CameraMapper
+from lsst.utils import getPackageDir
+import os
+
+
+class CompositeMapper(CameraMapper):
+    packageName = "obs_base"
+
+    def __init__(self, root, policyDir):
+        policy = dafPersist.Policy(os.path.join(policyDir, 'policy.yaml'))
+        super(CompositeMapper, self).__init__(policy, repositoryDir=root, root=root)
+
+    def _makeCamera(self, policy, repositoryDir):
+        """Normally this makes a camera (instance of lsst.afw.cameraGeom.Camera) describing the camera
+        geometry. For composite testing, we don't need a camera.
+        """
+        return None
