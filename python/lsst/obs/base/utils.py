623f604e793c3037e50b86b9d007cc07ba31ea19
John Swinbank <swinbank@princeton.edu>
2017-05-04 10:49:42 -0400

    Add utils.py.
    File to hold utility functions for obs_base type work. Currently contains
    bboxFromIraf() to convert IRAF style box defintions to an afwGeom.BoxI

--- python/lsst/obs/base/utils.py
+++ python/lsst/obs/base/utils.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import
 #
 # LSST Data Management System
-# Copyright 2008, 2009, 2010 LSST Corporation.
+# Copyright 2017 AURA/LSST
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -13,17 +12,27 @@ from __future__ import absolute_import
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-from .version import *
 
-from .mapping import *
-from .cameraMapper import *
-from .exposureIdInfo import *
-from .makeRawVisitInfo import *
-from .utils import *
+import re
+import lsst.afw.geom as afwGeom
+
+def bboxFromIraf(irafBBoxStr):
+    """Return a Box2I corresponding to an IRAF-style BBOX
+
+    [x0:x1,y0:y1] where x0 and x1 are the one-indexed start and end columns, and correspondingly
+    y0 and y1 are the start and end rows.
+    """
+
+    mat = re.search(r"^\[([-\d]+):([-\d]+),([-\d]+):([-\d]+)\]$", irafBBoxStr)
+    if not mat:
+        raise RuntimeError("Unable to parse IRAF-style bbox \"%s\"" % irafBBoxStr)
+    x0, x1, y0, y1 = [int(_) for _ in mat.groups()]
+
+    return afwGeom.BoxI(afwGeom.PointI(x0 - 1, y0 - 1), afwGeom.PointI(x1 - 1, y1 - 1))
