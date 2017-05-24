899e8202adc365f36adc5d5e1ba6e0db890d742a
Jim Bosch <jbosch@astro.princeton.edu>
2017-05-23 21:33:49 -0400

    Add unit test and fix SQL syntax bugs.

--- python/lsst/obs/base/repodb/common.py
+++ python/lsst/obs/base/repodb/common.py
@@ -10,7 +10,7 @@ class TractUnit(base.SpatialUnit):
     number = base.IntField()
     skymap = base.PythonTypeField()
     patches = base.ReverseForeignKey()
-    key = base.Tuple(number, skymap)
+    unique = (number, skymap)
 
 
 class PatchUnit(base.SpatialUnit):
@@ -18,23 +18,23 @@ class PatchUnit(base.SpatialUnit):
     skymap = base.Alias(tract, TractUnit.skymap)
     x = base.IntField()
     y = base.IntField()
-    key = base.Tuple(tract, x, y)
+    unique = (tract, x, y)
 
 
 class FilterUnit(base.Unit):
     name = base.StrField()
     camera = base.PythonTypeField()
     visits = base.ReverseForeignKey()
-    key = base.Tuple(name, camera)
+    unique = (name, camera)
 
 
 class VisitUnit(base.SpatialUnit):
     number = base.IntField()
     filter = base.ForeignKey(FilterUnit, reverse="visits")
-    camera = base.Alias(filter, FilterUnit.camera)
+    camera = base.PythonTypeField()
     sensors = base.ReverseForeignKey()
     dateobs = base.DateTimeField()
-    key = base.Tuple(number, camera)
+    unique = (number, camera)
 
 
 class SensorUnit(base.SpatialUnit):
@@ -44,7 +44,7 @@ class SensorUnit(base.SpatialUnit):
     filter = base.Alias(visit, VisitUnit.filter)
     dateobs = base.Alias(visit, VisitUnit.dateobs)
     raw = base.ReverseForeignKey()
-    key = base.Tuple(visit, number)
+    unique = (visit, number)
 
 
 class RawUnit(base.Unit):
@@ -54,7 +54,7 @@ class RawUnit(base.Unit):
     filter = base.Alias(sensor, SensorUnit.filter)
     dateobs = base.Alias(sensor, SensorUnit.dateobs)
     name = base.StrField()
-    key = base.Tuple(sensor, name)
+    unique = (sensor, name)
 
 
 class MasterCalibrationUnit(base.Unit):
@@ -62,7 +62,7 @@ class MasterCalibrationUnit(base.Unit):
     end = base.DateTimeField()
     filter = base.ForeignKey(FilterUnit, reverse=None, optional=True)
     camera = base.PythonTypeField()
-    key = base.Tuple(begin, end, filter, camera)
+    unique = (begin, end, filter, camera)
 
 
 COMMON_UNITS = (TractUnit, PatchUnit, FilterUnit, VisitUnit, SensorUnit,
