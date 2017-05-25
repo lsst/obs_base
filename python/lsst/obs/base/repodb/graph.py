aa6a46dc0b0282af212820af24976bbcb2731566
Jim Bosch <jbosch@astro.princeton.edu>
2017-05-25 16:03:45 -0400

    Big refactor to include Datasets in SQL db

--- python/lsst/obs/base/repodb/graph.py
+++ python/lsst/obs/base/repodb/graph.py
@@ -12,3 +12,34 @@ class RepoGraph:
         if datasets is None:
             datasets = {}
         self.datasets = datasets
+
+    def addDataset(self, DatasetClass, **units):
+        storage = {}
+        for k, p in DatasetClass.properties.items():
+            try:
+                v = units.pop(k)
+            except KeyError:
+                raise ValueError(
+                    "No value provided for {}.{}".format(
+                        type(self).__name__, k
+                    )
+                )
+            if not isinstance(v, p.UnitClass):
+                raise TypeError(
+                    "Invalid type (expected {}, got {}) for {}.{}".format(
+                        p.UnitClass.__name__,
+                        type(v),
+                        type(self).__name__,
+                        k
+                    )
+                )
+            storage[k] = v
+        if units:
+            raise ValueError(
+                "Unused values when constructing {}: {}".format(
+                    type(self).__name__,
+                    units.keys()
+                )
+            )
+        # TODO
+        raise NotImplementedError()
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -21,7 +21,7 @@ class GraphTestCase(unittest.TestCase):
     def testUnitUniverseGraph(self):
         graph = self.backend.makeGraph(repodb.COMMON_UNITS, ())
         self.assertItemsEqual(graph.units.keys(), repodb.COMMON_UNITS)
-        for UnitClass, names, values in repodb.tests.EXAMPLE_DATA:
+        for UnitClass, names, values in repodb.tests.EXAMPLE_UNITS:
             for unit, data in zip(graph.units[UnitClass], values):
                 for i, name in enumerate(names):
                     attr = getattr(unit, name)
@@ -39,16 +39,33 @@ class GraphTestCase(unittest.TestCase):
                             msg="{}.{}".format(UnitClass.__name__, name)
                         )
 
-    def testInMemoryDatasets(self):
-        CalExp = repodb.Dataset.subclass("CalExp", sensor=repodb.SensorUnit)
-        graph = self.backend.makeGraph(repodb.COMMON_UNITS, (CalExp,))
-        for sensor in graph.units[repodb.SensorUnit]:
-            graph.datasets[CalExp].add(CalExp(sensor=sensor))
+    def testNeededDatasets(self):
+        universe = self.backend.makeGraph(repodb.COMMON_UNITS)
+        graph = self.backend.makeGraph(repodb.COMMON_UNITS,
+                                       NeededDatasets=(repodb.tests.CalExp,))
         self.assertEqual(
-            set(calexp.sensor.id for calexp in graph.datasets[CalExp]),
+            set(calexp.sensor.id for calexp
+                in graph.datasets[repodb.tests.CalExp]),
             set(sensor.id for sensor in graph.units[repodb.SensorUnit])
         )
+        self.assertLess(
+            graph.units[repodb.SensorUnit],
+            universe.units[repodb.SensorUnit]
+        )
+        # TODO: test various linkages
 
+    def testFutureDatasets(self):
+        universe = self.backend.makeGraph(repodb.COMMON_UNITS)
+        graph = self.backend.makeGraph(repodb.COMMON_UNITS,
+                                       FutureDatasets=(repodb.tests.CalExp,))
+        self.assertLess(
+            set(calexp.sensor.id for calexp
+                in graph.datasets[repodb.tests.CalExp]),
+            set(sensor.id for sensor in graph.units[repodb.SensorUnit])
+        )
+        self.assertEqual(graph.units[repodb.SensorUnit],
+                         universe.units[repodb.SensorUnit])
+        # TODO: test various linkages
 
 
 if __name__ == "__main__":
