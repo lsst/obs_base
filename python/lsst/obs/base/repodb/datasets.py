6e338901927c9e65f86b2595f0a3614ad21d932c
Jim Bosch <jbosch@astro.princeton.edu>
2017-05-29 15:40:53 -0400

    Add documentation for Dataset and RepoGraph.

--- python/lsst/obs/base/repodb/datasets.py
+++ python/lsst/obs/base/repodb/datasets.py
@@ -6,6 +6,24 @@ __all__ = ("Dataset",)
 
 
 class UnitProperty(object):
+    """A descriptor class used to define `Unit` attributes for `Dataset`s
+
+    All `Unit` attributes on a `Dataset` class must be defined by
+    `UnitProperty` instances, and each `Dataset` class holds a `properties`
+    dictionary containing all `UnitProperty` instances for easy iteration.
+
+    Because `Dataset` types are not defined by users in the usual way, there
+    should be no need for users to construct them directly, and no reason to
+    interact with them other than for normal attribute access on instances.
+
+    Design Notes
+    ------------
+    Because all `UnitProperty` instances are created and added to their
+    `Dataset` subclasses by `DatasetMeta`, there's no need for the two-step
+    construct-attach pattern used by the `Unit` descriptors (which is used to
+    save the user from having to repeat the name of the attribute);
+    `UnitProperty` can just take its name in `__init__`.
+    """
 
     def __init__(self, name, UnitClass):
         self.name = name
@@ -17,6 +35,22 @@ class UnitProperty(object):
         return self
 
     def finalize(self, instance, units):
+        """Finish constructing a `Dataset` instance, translating integer ID
+        values in a `Dataset` instances `_storage` dict to the `Unit` instances
+        they corresond to.
+
+        Should only be called by `Backend` implementations.
+
+        Parameters
+        ----------
+        instance : `Dataset`
+            Instance of the `Dataset` type to which this property belongs.
+            Must have a `_storage` dict containing a integer IDs, not `Unit`
+            instances.
+        units : nested `dict` of `{UnitClass: {`int`: `Unit`}}
+            Nested dictionary of `Unit` instances, indexed first by `Unit` type
+            objects and then by integer ID.
+        """
         id = instance._storage.get(self.name, None)
         assert isinstance(id, int)
         assert id is not None
@@ -27,6 +61,16 @@ class UnitProperty(object):
 
 
 class DatasetMeta(type):
+    """A custom metaclass for `Dataset` and types derived from it.
+
+    `DatasetMeta` prevents construction of new `Dataset` types through the
+    `class` statement, ensuring consistent construction through
+    `Dataset.subclass`.  This includes registering all `Dataset` types in a
+    global registry to ensure `Dataset` type names are unique; trying to
+    define the same `Dataset` type in multiple places will return the same
+    type object, and trying to define different `Dataset`s with the same name
+    will raise `ValueError`.
+    """
 
     registry = {}
 
@@ -54,9 +98,44 @@ class DatasetMeta(type):
 
 
 class Dataset(with_metaclass(DatasetMeta, object)):
+    """An abstract base class for types that define data products.
+
+    A subclass of `Dataset` is conceptually just the combination of a
+    unique string name with a dictionary of `Unit` classes, and a
+    `Dataset` instance just provides values for those `Unit`s.
+
+    `Dataset` types may *only* be defined using the `subclass` static method
+    (not the usual Python `class` statement).  This reflects the fact that a
+    `Dataset` type is usually created dynamically by the `SuperTask`s that
+    produced it when the `SuperTask` is configured.
+
+    The `Dataset` instances in a `RepoDatabase` represent the actual data
+    products in that repository (or repositories chained to it).  It is
+    expected that `Butler.put` will be responsible for updating the
+    `RepoDatabase` when data products are added.  `Dataset` instances may be
+    added to `RepoGraph` before being added to `RepoDatabase` to represent
+    data products that *should* be produced by a `Pipeline` run.
+    """
 
     @staticmethod
     def subclass(name, **UnitClasses):
+        """Create a new type that inherits from `Dataset`.
+
+        Parameters
+        ----------
+        name : `str`
+            A unique name for this type.  If a `Dataset` type with this name
+            has already been created, that type will be returned instread of
+            creating a new one.
+
+        Additional keyword arguments are interpreted as the names and `Unit`
+        classes of the units of data that label the new `Dataset`.
+
+        Exceptions
+        ----------
+        Raises `ValueError` if another `Dataset` with the same name but
+        different units already exists.
+        """
         return DatasetMeta(name, **UnitClasses)
 
     def __init__(self, **kwds):
@@ -78,14 +157,38 @@ class Dataset(with_metaclass(DatasetMeta, object)):
         return "({}: {})".format(self.name, ", ".join(items))
 
     def getDataId(self):
+        """Return a dictionary-style data ID corresponding to this `Dataset`.
+
+        This is a temporary approach that allows `Dataset` types to be used
+        with the existing `Mapper`/`Butler` implementations before they have
+        been integrated with the `RepoDatabase` system.  It should not be
+        permanent.
+        """
         return {p.name: p.__get__(self).getDataIdValue()
                 for p in self.properties.values()}
 
     def get(self, butler, **kwds):
+        """Read a `Dataset` using a `Butler`.
+
+        This is a temporary interface that should be replaced by a version
+        of `Butler.get` that accepts a `Dataset` instance in the future.
+        """
         return butler.get(self.name, self.getDataId(), **kwds)
 
     def put(self, butler, value, **kwds):
+        """Write a `Dataset` using a `Butler`.
+
+        This is a temporary interface that should be replaced by a version
+        of `Butler.put` that accepts a `Dataset` instance in the future.
+        """
         return butler.put(value, self.name, self.getDataId(), **kwds)
 
     def exists(self, butler):
+        """Test whether a `Dataset` exists on disk using a `Butler`.
+
+        This is a temporary interface that delegates to `Butler.datasetExists`.
+        In the future, `Butler` should guarantee that any `Dataset` in a
+        `RepoDatabase` exists on disk, and this method may not need to exist
+        at all.
+        """
         return butler.datasetExists(self.name, self.getDataId())
--- python/lsst/obs/base/repodb/graph.py
+++ python/lsst/obs/base/repodb/graph.py
@@ -4,6 +4,41 @@ __all__ = ("RepoGraph",)
 
 
 class RepoGraph:
+    """A view of the content in a data repository as a graph of `Unit` and
+    `Dataset` instances.
+
+    The connections between `Unit`s (via `ForeignKey` descriptors) and
+    `Datasets` (via `UnitProperty` descriptors) naturally forms a graph-like
+    data structure.  A `RepoGraph` is the conceptual owner of a collection of
+    related `Unit` and `Dataset` instances, representing a (possibly
+    restricted) view of the content in a data repository.
+
+    `RepoGraphs` should generally only be constructed by the `makeGraph`
+    method of `RepoDatabase`, which ensures all `Unit`s and `Dataset`s have
+    the full set of links that relate them.  After construction, new
+    `Datasets` instances may be added to the graph using `addDataset`
+    (typically this is done to represent a `Dataset` a `SuperTask` is expected
+    to produce), but the set of `Unit` instances (as well as the types of
+    `Unit`s and `Dataset`s) is considered fixed.
+
+    A `RepoGraph` may be restricted relative to its parent `RepoDatabase` in
+    three ways:
+
+     - The set of `Unit` types may be a subset of the `Unit` types in the
+       `RepoDatabase`.
+
+     - The set of `Dataset` types may be a subset of the `Dataset` types in the
+       `RepoDatabase`.
+
+     - The set of `Unit` instances may be a subset of the `Unit` instances in
+       the `RepoDatabaes`, which naturally restricts the `Dataset` instances
+       in the graph to only those with `Unit`s in the restricted set.
+
+    All of these restrictions must be consistent: all `Unit` types (instances)
+    used by any `Dataset` type (instance) in the graph must be present in the
+    graph, as well as any `Unit` type (instance) referred to by a `ForeignKey`
+    field by a `Unit` type (instance) otherwise required.
+    """
 
     def __init__(self, units=None, datasets=None):
         if units is None:
@@ -14,6 +49,26 @@ class RepoGraph:
         self.datasets = datasets
 
     def addDataset(self, DatasetClass, **units):
+        """Create a new `Dataset` instance and add it to the graph.
+
+        If a `Dataset` instance with the given name and units already exists
+        in the graph, an equivalent `Dataset` instance will be returned that
+        and the graph will not be modified (the returned instance may not be
+        the same in-memory object that is present in the graph, but the two
+        will compare as equal and should otherwise be indistinguishable).
+
+        Parameters
+        ----------
+        DatasetClass : `type` inherited from `Dataset`.
+            A type object for the instance about to be constructed.
+
+        Additional keyword parameters contain the names (keys) and `Unit`
+        instances (values) that label the `Dataset`.
+
+        Returns
+        -------
+        A new instance of `DatasetClass`.
+        """
         storage = {}
         for k, p in DatasetClass.properties.items():
             try:
