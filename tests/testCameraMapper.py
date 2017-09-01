b2174f4d3b7017c2a929e14589ce286c60ccef07
Nathan Pease <npease@slac.stanford.edu>
2017-09-01 14:38:28 -0700

    don't delete everything, gc in teardown is sufficient

--- tests/testCameraMapper.py
+++ tests/testCameraMapper.py
@@ -439,9 +439,6 @@ class ParentRegistryTestCase(unittest.TestCase):
         registryA = butler._repos.inputs()[0].repo._mapper.registry
         registryB = butler._repos.outputs()[0].repo._mapper.registry
         self.assertEqual(id(registryA), id(registryB))
-        del registryA
-        del registryB
-        del butler
 
         self._createRegistry(os.path.join(repoBRoot, 'registry.sqlite3'))
         butler = dafPersist.Butler(inputs=self.repoARoot, outputs=repoBRoot)
@@ -449,9 +446,6 @@ class ParentRegistryTestCase(unittest.TestCase):
         registryA = butler._repos.inputs()[0].repo._mapper.registry
         registryB = butler._repos.outputs()[0].repo._mapper.registry
         self.assertNotEqual(id(registryA), id(registryB))
-        del registryA
-        del registryB
-        del butler
 
 class MissingPolicyKeyTestCase(unittest.TestCase):
 
