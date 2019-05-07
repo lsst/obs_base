bbd2bc9f4c83a719df479158dd323b85ca6f4d0d
Simon Krughoff <krughoff@lsst.org>
2019-05-07 00:18:29 +0000

    Add parts of calib_id as proper metadata as well

--- python/lsst/obs/base/read_defects.py
+++ python/lsst/obs/base/read_defects.py
@@ -5,7 +5,7 @@ from dateutil import parser
 
 
 def read_defects_one_chip(root, chip_name, camera):
-    files = glob.glob(os.path.join(root, chip_name, '*.dat'))
+    files = glob.glob(os.path.join(root, chip_name.lower(), '*.dat'))
     parts = os.path.split(root)
     instrument = os.path.split(parts[0])[1]  # convention is that these reside at <instrument>/defects
     defect_dict = {}
@@ -20,12 +20,16 @@ def read_defects_one_chip(root, chip_name, camera):
 def fix_up_metadata(defects, valid_start, instrument, chip_id):
     md = defects.getMetadata()
     md['INSTRUME'] = instrument
+    md['DETECTOR'] = chip_id
+    md['CALIBDATE'] = valid_start.isoformat()
+    md['FILTER'] = None
     md['CALIB_ID'] = f'detector={chip_id} calibDate={valid_start.isoformat()} ccd={chip_id} ccdnum={chip_id} filter=None'
 
 def read_all_defects(root, camera):
     dirs = os.listdir(root)  # assumes all directories contain defects
     defects_by_chip = {}
+    name_map = {det.getName().lower():det.getName() for det in camera}  # we assume the directories have been lowered
     for d in dirs:
-        chip_name = os.path.basename(d)
+        chip_name = name_map[os.path.basename(d)]
         defects_by_chip[chip_name] = read_defects_one_chip(root, chip_name, camera)
     return defects_by_chip
