9af8f464a02331bbd9aa872d8f5d21dbbb9e8cce
Nathan Pease <npease@slac.stanford.edu>
2020-04-11 19:30:38 -0500

    add instrument option

---
+++ python/lsst/obs/base/cli/opt/instrument.py
@@ -0,0 +1,34 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import click
+
+
+class instrument_option:  # noqa: N801
+    def __init__(self, required=False, helpMsg=None):
+        self.required = required
+        self.help = helpMsg if helpMsg is not None else "The name of an Instrument subclass."
+
+    def __call__(self, f):
+        return click.option("-i", "--instrument",
+                            required=self.required,
+                            help=self.help)(f)
