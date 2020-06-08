efa299e6a9a424738ffae2fc3c201c4aa6444915
Nathan Pease <npease@slac.stanford.edu>
2020-06-08 15:49:13 -0500

    add register instruments test

---
+++ tests/test_cliCmdRegisterInstrument.py
@@ -0,0 +1,48 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests for daf_butler CLI define-visits command.
+"""
+
+import unittest
+
+from lsst.daf.butler.tests.mockeredTest import MockeredTestBase
+
+
+class RegisterInstrumentTest(MockeredTestBase):
+
+    defaultExpected = dict()
+
+    def test_repoBasic(self):
+        """Test the most basic required arguments."""
+        self.run_test(["register-instrument", "here",
+                       "--instrument", "a.b.c"],
+                      self.makeExpected(repo="here",
+                                        instrument="a.b.c"))
+
+    def test_missing(self):
+        """test a missing argument"""
+        self.run_missing(["register-instrument"], 'Missing argument "REPO"')
+        self.run_missing(["register-instrument", "here"], 'Missing option "-i" / "--instrument"')
+
+
+if __name__ == "__main__":
+    unittest.main()
