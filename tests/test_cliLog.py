5c7b4d196fb6255d0996d3b743bf8fb5d9ed6603
Nathan Pease <npease@slac.stanford.edu>
2020-07-13 13:05:16 -0700

    test the butler CliLog
    This test also runs in butler, but butler does not depend on
    lsst.log so in CI those parts of CliLog are not tested with daf_butler.
    Since obs_base does depend on lsst.log, running the test here is
    more complete.

--- tests/test_cliLog.py
+++ tests/test_cliLog.py
@@ -19,57 +19,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for the daf_butler cli.Log utility that test code depenent on
-lsst.log. Code is implemented in daf_butler only runs if lsst.log.Log can be
-imported. It can't be tested there because daf_butler does not directly depend
-on lsst.log, and only uses it if it has been setup by another package."""
+"""Unit tests for the daf_butler CliLog utility. Code is implemented in
+daf_butler but some only runs if lsst.log.Log can be imported so these parts of
+it can't be tested there because daf_butler does not directly depend on
+lsst.log, and only uses it if it has been setup by another package."""
 
-import logging
 import unittest
 
-from lsst.daf.butler.cli.cmd import create
-from lsst.daf.butler.cli.log import Log as butlerCliLog
-from lsst.daf.butler.tests import CliCmdTestBase
-from lsst.log import Log as lsstLog
+from lsst.daf.butler.cli.cliLog import CliLog
+from lsst.daf.butler.tests import CliLogTestBase
+import lsst.log
 
 
-class ConvertLogLevelTestCase(CliCmdTestBase,
-                              unittest.TestCase):
-    """Test that the log levels accepted by the log_level_option are translated
-    to lsst.log levels correctly.
+class CliLogTestCase(CliLogTestBase,
+                     unittest.TestCase):
+    """Test log initialization, reset, and setting log levels on python
+    `logging` and `lsst.log`.
 
-    """
+    This test also runs in daf_butler but will not test `lsst.log` in CI
+    because daf_butler does not directly depend on that package."""
+    pass
 
-    command = create
-    defaultExpected = dict(repo=None,
-                           seed_config=None,
-                           standalone=False,
-                           override=False,
-                           outfile=None)
 
-    def test_logInit(self):
-        """Initialize logging by running a butler command. Verify a handler
-        has been added"""
-        rootLogger = logging.getLogger()
-        self.assertEqual(len(rootLogger.handlers), 0,
-                         msg="The root logger should not have any handlers when this test is started.")
-        self.run_test(["--log-level", "WARNING", "create", "foo"],
-                      self.makeExpected(repo="foo"))
-        self.assertEqual(len(rootLogger.handlers), 1,
-                         msg="After running a butler command the root loger should have been initialized "
-                             "with the lsst logger handler.")
-        butlerCliLog.uninitLog()
-        self.assertEqual(len(rootLogger.handlers), 0,
-                         msg="After uninit the lsst logger handler should have been removed from the root "
-                             "logger.")
-
+class ConvertLsstLogLevelTestCase(unittest.TestCase):
 
     def test_convertToLsstLogLevel(self):
-        self.assertEqual(lsstLog.FATAL, butlerCliLog.getLsstLogLevel("CRITICAL"))
-        self.assertEqual(lsstLog.ERROR, butlerCliLog.getLsstLogLevel("ERROR"))
-        self.assertEqual(lsstLog.WARN, butlerCliLog.getLsstLogLevel("WARNING"))
-        self.assertEqual(lsstLog.INFO, butlerCliLog.getLsstLogLevel("INFO"))
-        self.assertEqual(lsstLog.DEBUG, butlerCliLog.getLsstLogLevel("DEBUG"))
+        """Test that the log levels accepted by the log_level_option are
+        translated to lsst.log levels correctly."""
+        self.assertEqual(lsst.log.Log.FATAL, CliLog._getLsstLogLevel("CRITICAL"))
+        self.assertEqual(lsst.log.ERROR, CliLog._getLsstLogLevel("ERROR"))
+        self.assertEqual(lsst.log.WARN, CliLog._getLsstLogLevel("WARNING"))
+        self.assertEqual(lsst.log.INFO, CliLog._getLsstLogLevel("INFO"))
+        self.assertEqual(lsst.log.DEBUG, CliLog._getLsstLogLevel("DEBUG"))
 
 
 if __name__ == "__main__":
