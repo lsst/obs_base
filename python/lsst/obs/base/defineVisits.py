29d990e688b4cae4326c4829a21c171767b30e3a
Tim Jenness <tjenness@lsst.org>
2020-08-12 14:56:30 -0700

    Add support for calculating WCS from registry and formatter
    Falls back to old scheme if the registry doesn't include
    ra/dec
    
    This requires that the compute routine can request the
    Instrument class.

--- python/lsst/obs/base/defineVisits.py
+++ python/lsst/obs/base/defineVisits.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
 
 __all__ = [
     "DefineVisitsConfig",
@@ -42,12 +43,14 @@ from lsst.daf.butler import (
     Timespan,
     TIMESPAN_FIELD_SPECS,
 )
+
+import lsst.geom
 from lsst.geom import Box2D
 from lsst.pex.config import Config, Field, makeRegistry, registerConfigurable
 from lsst.afw.cameraGeom import FOCAL_PLANE, PIXELS
 from lsst.pipe.base import Task
 from lsst.sphgeom import ConvexPolygon, Region, UnitVector3d
-from ._instrument import loadCamera
+from ._instrument import loadCamera, Instrument
 
 
 @dataclasses.dataclass
@@ -185,12 +188,15 @@ class ComputeVisitRegionsTask(Task, metaclass=ABCMeta):
     ----------
     config : `ComputeVisitRegionsConfig`
         Configuration information.
+    butler : `lsst.daf.butler.Butler`
+        The butler to use.
     **kwargs
         Additional keyword arguments forwarded to the `Task` constructor.
     """
     def __init__(self, config: ComputeVisitRegionsConfig, *, butler: Butler, **kwargs: Any):
         Task.__init__(self, config=config, **kwargs)
         self.butler = butler
+        self.instrumentMap = {}
 
     ConfigClass = ComputeVisitRegionsConfig
 
@@ -201,6 +207,30 @@ class ComputeVisitRegionsTask(Task, metaclass=ABCMeta):
              "and visit+detector combinations."),
         configBaseType=ComputeVisitRegionsConfig,
     )
+
+    def getInstrument(self, instrumentName) -> Instrument:
+        """Retrieve an `~lsst.obs.base.Instrument` associated with this
+        instrument name.
+
+        Parameters
+        ----------
+        instrumentName : `str`
+            The name of the instrument.
+
+        Returns
+        -------
+        instrument : `~lsst.obs.base.Instrument`
+            The associated instrument object.
+
+        Notes
+        -----
+        The result is cached.
+        """
+        instrument = self.instrumentMap.get(instrumentName)
+        if instrument is None:
+            instrument = Instrument.fromName(instrumentName, self.butler.registry)
+            self.instrumentMap[instrumentName] = instrument
+        return instrument
 
     @abstractmethod
     def compute(self, visit: VisitDefinitionData, *, collections: Any = None
@@ -624,18 +654,43 @@ class _ComputeVisitRegionsFromSingleRawWcsTask(ComputeVisitRegionsTask):
         camera, versioned = loadCamera(self.butler, exposure.dataId, collections=collections)
         if not versioned and self.config.requireVersionedCamera:
             raise LookupError(f"No versioned camera found for exposure {exposure.dataId}.")
-        if self.config.detectorId is None:
-            wcsRefs = list(self.butler.registry.queryDatasets("raw.wcs", dataId=exposure.dataId,
-                                                              collections=collections))
-            if not wcsRefs:
-                raise LookupError(f"No raw.wcs datasets found for data ID {exposure.dataId} "
-                                  f"in collections {collections}.")
-            wcsDetector = camera[wcsRefs[0].dataId["detector"]]
-            wcs = self.butler.getDirect(wcsRefs[0])
+
+        # Derive WCS from boresight information -- if available in registry
+        use_registry = True
+        try:
+            orientation = lsst.geom.Angle(exposure.sky_angle, lsst.geom.degrees)
+            radec = lsst.geom.SpherePoint(lsst.geom.Angle(exposure.tracking_ra, lsst.geom.degrees),
+                                          lsst.geom.Angle(exposure.tracking_dec, lsst.geom.degrees))
+        except AttributeError:
+            use_registry = False
+
+        if use_registry:
+            if self.config.detectorId is None:
+                detectorId = next(camera.getIdIter())
+            else:
+                detectorId = self.config.detectorId
+            wcsDetector = camera[detectorId]
+
+            # Ask the raw formatter to create the relevant WCS
+            # This allows flips to be taken into account
+            instrument = self.getInstrument(exposure.instrument)
+            rawFormatter = instrument.getRawFormatter({"detector": detectorId})
+            wcs = rawFormatter.makeRawSkyWcsFromBoresight(radec, orientation, wcsDetector)
+
         else:
-            wcsDetector = camera[self.config.detectorId]
-            wcs = self.butler.get("raw.wcs", dataId=exposure.dataId, detector=self.config.detectorId,
-                                  collections=collections)
+            if self.config.detectorId is None:
+                wcsRefsIter = self.butler.registry.queryDatasets("raw.wcs", dataId=exposure.dataId,
+                                                                 collections=collections)
+                if not wcsRefsIter:
+                    raise LookupError(f"No raw.wcs datasets found for data ID {exposure.dataId} "
+                                      f"in collections {collections}.")
+                wcsRef = next(iter(wcsRefsIter))
+                wcsDetector = camera[wcsRef.dataId["detector"]]
+                wcs = self.butler.getDirect(wcsRef)
+            else:
+                wcsDetector = camera[self.config.detectorId]
+                wcs = self.butler.get("raw.wcs", dataId=exposure.dataId, detector=self.config.detectorId,
+                                      collections=collections)
         fpToSky = wcsDetector.getTransform(FOCAL_PLANE, PIXELS).then(wcs.getTransform())
         bounds = {}
         for detector in camera:
