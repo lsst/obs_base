e89227eaef8549d5f56df1c74064b619e0236304
Jim Bosch <jbosch@astro.princeton.edu>
2021-05-03 14:34:23 -0400

    Move some raw formatter logic into a helper method.
    This will remove some duplication between the base and obs_lsst
    implementations, and should avoid more duplication here in the near
    future.

--- python/lsst/obs/base/_fitsRawFormatterBase.py
+++ python/lsst/obs/base/_fitsRawFormatterBase.py
@@ -359,13 +359,7 @@ class FitsRawFormatterBase(Formatter):
         from lsst.afw.image import makeExposure, makeMaskedImage
         full = makeExposure(makeMaskedImage(self.readImage()))
         full.setDetector(self.getDetector(self.observationInfo.detector_num))
-        info = full.getInfo()
-        info.setFilterLabel(self.makeFilterLabel())
-        info.setVisitInfo(self.makeVisitInfo())
-        info.setWcs(self.makeWcs(info.getVisitInfo(), info.getDetector()))
-        # We don't need to call stripMetadata() here because it has already
-        # been stripped during creation of the ObservationInfo, WCS, etc.
-        full.setMetadata(self.metadata)
+        self.attachComponentsFromMetadata(full)
         return full
 
     def read(self, component=None):
@@ -420,3 +414,21 @@ class FitsRawFormatterBase(Formatter):
             self._observationInfo = ObservationInfo(self.metadata, translator_class=self.translatorClass,
                                                     filename=path)
         return self._observationInfo
+
+    def attachComponentsFromMetadata(self, exposure):
+        """Attach all `lsst.afw.image.Exposure` components derived from
+        metadata (including the stripped metadata itself).
+
+        Parameters
+        ----------
+        exposure : `lsst.afw.image.Exposure`
+            Exposure to attach components to (modified in place).  Must already
+            have a detector attached.
+        """
+        info = exposure.getInfo()
+        info.setFilterLabel(self.makeFilterLabel())
+        info.setVisitInfo(self.makeVisitInfo())
+        info.setWcs(self.makeWcs(info.getVisitInfo(), info.getDetector()))
+        # We don't need to call stripMetadata() here because it has already
+        # been stripped during creation of the ObservationInfo, WCS, etc.
+        exposure.setMetadata(self.metadata)
