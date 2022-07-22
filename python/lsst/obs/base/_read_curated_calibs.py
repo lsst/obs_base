9ee073c84c0027ec14cc03f496f518312d9ae66c
Tim Jenness <tjenness@lsst.org>
2022-07-22 14:08:55 -0400

    Reformat code transferred from pipe_tasks

--- python/lsst/obs/base/_read_curated_calibs.py
+++ python/lsst/obs/base/_read_curated_calibs.py
@@ -1,9 +1,9 @@
-from lsst.meas.algorithms.simple_curve import Curve
-from lsst.ip.isr import (Linearizer, CrosstalkCalib, Defects, BrighterFatterKernel, PhotodiodeCalib)
-
-import os
 import glob
+import os
+
 import dateutil.parser
+from lsst.ip.isr import BrighterFatterKernel, CrosstalkCalib, Defects, Linearizer, PhotodiodeCalib
+from lsst.meas.algorithms.simple_curve import Curve
 
 
 def read_one_chip(root, chip_name, chip_id):
@@ -25,9 +25,14 @@ def read_one_chip(root, chip_name, chip_id):
         A dictionary of objects constructed from the appropriate factory class.
         The key is the validity start time as a `datetime` object.
     """
-    factory_map = {'qe_curve': Curve, 'defects': Defects, 'linearizer': Linearizer,
-                   'crosstalk': CrosstalkCalib, 'bfk': BrighterFatterKernel,
-                   'photodiode': PhotodiodeCalib, }
+    factory_map = {
+        "qe_curve": Curve,
+        "defects": Defects,
+        "linearizer": Linearizer,
+        "crosstalk": CrosstalkCalib,
+        "bfk": BrighterFatterKernel,
+        "photodiode": PhotodiodeCalib,
+    }
     files = []
     extensions = (".ecsv", ".yaml")
     for ext in extensions:
@@ -36,8 +41,10 @@ def read_one_chip(root, chip_name, chip_id):
     instrument = os.path.split(parts[0])[1]  # convention is that these reside at <instrument>/<data_name>
     data_name = parts[1]
     if data_name not in factory_map:
-        raise ValueError(f"Unknown calibration data type, '{data_name}' found. "
-                         f"Only understand {','.join(k for k in factory_map)}")
+        raise ValueError(
+            f"Unknown calibration data type, '{data_name}' found. "
+            f"Only understand {','.join(k for k in factory_map)}"
+        )
     factory = factory_map[data_name]
     data_dict = {}
     for f in files:
@@ -78,16 +85,18 @@ def check_metadata(obj, valid_start, instrument, chip_id, filepath, data_name):
         in the path do not match for any reason.
     """
     md = obj.getMetadata()
-    finst = md['INSTRUME']
-    fchip_id = md['DETECTOR']
-    fdata_name = md['OBSTYPE']
-    if not ((finst.lower(), int(fchip_id), fdata_name.lower())
-            == (instrument.lower(), chip_id, data_name.lower())):
-        raise ValueError(f"Path and file metadata do not agree:\n"
-                         f"Path metadata: {instrument} {chip_id} {data_name}\n"
-                         f"File metadata: {finst} {fchip_id} {fdata_name}\n"
-                         f"File read from : %s\n"%(filepath)
-                         )
+    finst = md["INSTRUME"]
+    fchip_id = md["DETECTOR"]
+    fdata_name = md["OBSTYPE"]
+    if not (
+        (finst.lower(), int(fchip_id), fdata_name.lower()) == (instrument.lower(), chip_id, data_name.lower())
+    ):
+        raise ValueError(
+            f"Path and file metadata do not agree:\n"
+            f"Path metadata: {instrument} {chip_id} {data_name}\n"
+            f"File metadata: {finst} {fchip_id} {fdata_name}\n"
+            f"File read from : %s\n" % (filepath)
+        )
 
 
 def read_all(root, camera):
@@ -117,8 +126,9 @@ def read_all(root, camera):
     dirs = os.listdir(root)  # assumes all directories contain data
     dirs = [d for d in dirs if os.path.isdir(os.path.join(root, d))]
     data_by_chip = {}
-    name_map = {det.getName().lower(): det.getName() for
-                det in camera}  # we assume the directories have been lowered
+    name_map = {
+        det.getName().lower(): det.getName() for det in camera
+    }  # we assume the directories have been lowered
 
     if not dirs:
         raise RuntimeError(f"No data found on path {root}")
@@ -136,13 +146,15 @@ def read_all(root, camera):
                 # report example subset
                 note_str = "examples"
                 detectors = detectors[:max_detectors]
-            raise RuntimeError(f"Detector {chip_name} not known to supplied camera "
-                               f"{camera.getName()} ({note_str}: {','.join(detectors)})")
+            raise RuntimeError(
+                f"Detector {chip_name} not known to supplied camera "
+                f"{camera.getName()} ({note_str}: {','.join(detectors)})"
+            )
         chip_id = camera[name_map[chip_name]].getId()
         data_by_chip[chip_name], calib_type = read_one_chip(root, chip_name, chip_id)
         calib_types.add(calib_type)
         if len(calib_types) != 1:  # set.add(None) has length 1 so None is OK here.
-            raise ValueError(f'Error mixing calib types: {calib_types}')
+            raise ValueError(f"Error mixing calib types: {calib_types}")
 
     no_data = all([v == {} for v in data_by_chip.values()])
     if no_data:
