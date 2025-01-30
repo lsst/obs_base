8c60ae754904c3b1852cb546e9aa768d62f0c519
Tim Jenness <tjenness@lsst.org>
2025-01-30 12:35:42 -0700

    Fix docstrings for new black

--- tests/test_cliCmdTestIngest.py
+++ tests/test_cliCmdTestIngest.py
@@ -19,8 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Unit tests for daf_butler CLI ingest-raws command.
-"""
+"""Unit tests for daf_butler CLI ingest-raws command."""
 
 import unittest
 
