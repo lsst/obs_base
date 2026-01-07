3f2197ea4d76c92455ea036a4d7b56989d2c5736
Jim Bosch <jbosch@astro.princeton.edu>
2026-01-07 12:05:25 -0500

    Add Python interface for updating dimension regions in repos.

--- python/lsst/obs/base/visit_geometry.py
+++ python/lsst/obs/base/visit_geometry.py
@@ -23,10 +23,16 @@ from __future__ import annotations
 
 __all__ = ("VisitGeometry",)
 
+import itertools
+from typing import Any
 
 import pydantic
 
+from lsst.daf.butler import Butler, DimensionRecord
 from lsst.daf.butler.pydantic_utils import SerializableRegion
+from lsst.utils.logging import getLogger
+
+_LOG = getLogger(__name__)
 
 
 class VisitGeometry(pydantic.BaseModel):
@@ -44,10 +50,104 @@ class VisitGeometry(pydantic.BaseModel):
     """Re-fit boresight declination in degrees."""
 
     orientation: float
-    """Re-fit rotation angle in degress."""
+    """Re-fit rotation angle in degrees."""
 
     visit_region: SerializableRegion
     """Updated region for the visit."""
 
     detector_regions: dict[int, SerializableRegion] = pydantic.Field(default_factory=dict)
     """Updated region for each detector in this visit."""
+
+    @classmethod
+    def update_dimension_records(
+        cls,
+        butler: Butler,
+        instrument: str,
+        *where: Any,
+        dataset_type: str = "visit_geometry",
+        batch_size: int = 100,
+        **kwargs: Any,
+    ) -> int:
+        """Update the dimension records in a data repository by querying for
+        and reading visit geometry datasets.
+
+        Parameters
+        ----------
+        butler : `lsst.daf.butler.Butler`
+            Butler client.  Must be writable and have its default collections
+            set to the location of the visit geometry datasets.
+        instrument : `str`
+            Name of the instrument whose records will be updated.
+        *where
+            Positional argument query constraint terms, forwarded to
+            `lsst.daf.butler.queries.Query.where` (e.g. an expression string or
+            data ID).
+        dataset_type : `str`, optional
+            Dataset type name for the visit geometry datasets.
+        batch_size : `int`, optional
+            Number of visits to process at once.
+        **kwargs
+            Keyword-only query constraint terms, forwarded to
+            `lsst.daf.butler.queries.Query.where` (e.g. data ID key-value
+            pairs).
+
+        Returns
+        -------
+        n_visits : `int`
+            The number of visits updated.
+        """
+        with butler.query() as query:
+            query = query.join_dataset_search(dataset_type)
+            query = query.where(instrument=instrument, *where, **kwargs)
+            _LOG.verbose("Querying for %s datasets.", dataset_type)
+            all_refs = list(query.datasets(dataset_type))
+            all_refs.sort(key=lambda ref: ref.dataId["visit"])
+            _LOG.verbose("Querying for visit records (%d expected).", len(all_refs))
+            old_visit_records = {r.id: r for r in query.dimension_records("visit")}
+            _LOG.verbose("Querying for exposure records (%d expected).", len(all_refs))
+            old_exposure_records = {r.id: r for r in query.dimension_records("exposure")}
+        vdr_record_cls = butler.dimensions.elements["visit_detector_region"].RecordClass
+        for ref_batch in itertools.batched(all_refs, batch_size):
+            _LOG.verbose(
+                "Updating records for %d visits with IDs between %d and %d.",
+                len(ref_batch),
+                ref_batch[0].dataId["visit"],
+                ref_batch[-1].dataId["visit"],
+            )
+            new_exposure_records: list[DimensionRecord] = []
+            new_visit_records: list[DimensionRecord] = []
+            new_vdr_records: list[DimensionRecord] = []
+            for ref in ref_batch:
+                visit_id = ref.dataId["visit"]
+                visit_geometry: VisitGeometry = butler.get(ref)
+                new_exposure_records.append(
+                    cls._make_updated_dimension_record(
+                        old_exposure_records[visit_id],
+                        tracking_ra=visit_geometry.boresight_ra,
+                        tracking_dec=visit_geometry.boresight_dec,
+                        sky_angle=visit_geometry.orientation,
+                    )
+                )
+                new_visit_records.append(
+                    cls._make_updated_dimension_record(
+                        old_visit_records[visit_id], region=visit_geometry.visit_region
+                    )
+                )
+                for detector_id, vdr_region in visit_geometry.detector_regions.items():
+                    new_vdr_records.append(
+                        vdr_record_cls(
+                            instrument=instrument, visit=visit_id, detector=detector_id, region=vdr_region
+                        )
+                    )
+            with butler.transaction():
+                butler.registry.insertDimensionData("exposure", *new_exposure_records, replace=True)
+                butler.registry.insertDimensionData("visit", *new_visit_records, replace=True)
+                butler.registry.insertDimensionData("visit_detector_region", *new_vdr_records, replace=True)
+        _LOG.info("Records for %d visits updated.", len(all_refs))
+        return len(all_refs)
+
+    @staticmethod
+    def _make_updated_dimension_record(original: DimensionRecord, **kwargs: Any) -> DimensionRecord:
+        as_dict = original.toDict()
+        as_dict.update(**kwargs)
+        return type(original)(**as_dict)
--- tests/test_visit_geometry.py
+++ tests/test_visit_geometry.py
@@ -19,22 +19,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import tempfile
 import unittest
 
+import numpy as np
+
+from lsst.daf.butler import Butler, DatasetType, DimensionRecord
 from lsst.obs.base.visit_geometry import VisitGeometry
-from lsst.pipe.base.tests.mocks import InMemoryRepo
+from lsst.sphgeom import Angle, ConvexPolygon, LonLat, UnitVector3d
 
 
 class VisitGeometryTestCase(unittest.TestCase):
     """Tests for VisitGeometry."""
 
     def setUp(self):
-        self.repo: InMemoryRepo = self.enterContext(InMemoryRepo("base.yaml", "spatial.yaml"))
+        root = self.enterContext(tempfile.TemporaryDirectory(ignore_cleanup_errors=True))
+        Butler.makeRepo(root)
+        self.butler: Butler = self.enterContext(Butler.from_config(root, run="geometry"))
+        self.rng = np.random.default_rng(500)
+        self.rot_about = UnitVector3d(*self.rng.uniform(0.0, 1.0, size=3).tolist())
+        self.rot_amount = Angle.fromDegrees(self.rng.uniform(0.0, 5.0))
 
     def test_round_trip(self):
         """Test round-tripping through a JSON string."""
-        (visit_record,) = self.repo.butler.query_dimension_records("visit", visit=1, instrument="Cam1")
-        visit_detector_region_records = self.repo.butler.query_dimension_records(
+        self.butler.import_(filename="resource://lsst.daf.butler/tests/registry_data/base.yaml")
+        self.butler.import_(filename="resource://lsst.daf.butler/tests/registry_data/spatial.yaml")
+        (visit_record,) = self.butler.query_dimension_records("visit", visit=1, instrument="Cam1")
+        visit_detector_region_records = self.butler.query_dimension_records(
             "visit_detector_region", visit=1, instrument="Cam1"
         )
         vg1 = VisitGeometry(
@@ -51,6 +62,93 @@ class VisitGeometryTestCase(unittest.TestCase):
         self.assertEqual(vg1.orientation, vg2.orientation)
         self.assertEqual(vg1.visit_region, vg2.visit_region)
         self.assertEqual(vg1.detector_regions, vg2.detector_regions)
+
+    def test_update_dimension_records(self):
+        """Test the update_dimension_records method."""
+        self.butler.import_(filename="resource://lsst.daf.butler/tests/registry_data/base.yaml")
+        self.butler.import_(filename="resource://lsst.daf.butler/tests/registry_data/spatial.yaml")
+        old_visit_records = {r.id: r for r in self.butler.query_dimension_records("visit")}
+        old_exposure_records = {
+            r.id: self._invent_and_insert_exposure_record(r) for r in old_visit_records.values()
+        }
+        self.butler.registry.registerDatasetType(
+            DatasetType("visit_geometry", self.butler.dimensions.conform(["visit"]), "VisitGeometry")
+        )
+        visit_geometries: dict[int, VisitGeometry] = {}
+        self.assertEqual(len(old_visit_records), 2)
+        for visit_id, visit_record in old_visit_records.items():
+            exposure_record = old_exposure_records[visit_id]
+            old_boresight = LonLat.fromDegrees(exposure_record.tracking_ra, exposure_record.tracking_dec)
+            new_boresight = LonLat(self._modify_point(UnitVector3d(old_boresight)))
+            visit_geometry = VisitGeometry(
+                boresight_ra=new_boresight.getLon().asDegrees(),
+                boresight_dec=new_boresight.getLat().asDegrees(),
+                # We *mostly* try to modify the geometry self-consistently, but
+                # the orientation angle is just arbitrary.  Nothing in the test
+                # right now requires any kind of consistency between the
+                # regions and the exposure angle fields.
+                orientation=self.rng.uniform(0.0, 5.0),
+                visit_region=self._modify_polygon(visit_record.region),
+                detector_regions={
+                    r.detector: self._modify_polygon(r.region)
+                    for r in self.butler.query_dimension_records(
+                        "visit_detector_region", data_id=visit_record.dataId
+                    )
+                },
+            )
+            visit_geometries[visit_id] = visit_geometry
+            self.butler.put(visit_geometry, "visit_geometry", visit_record.dataId)
+        n_updated = VisitGeometry.update_dimension_records(self.butler, instrument="Cam1")
+        self.assertEqual(n_updated, 2)
+        for new_visit_record in self.butler.query_dimension_records("visit"):
+            visit_id = new_visit_record.id
+            old_visit_record = old_visit_records[visit_id]
+            visit_geometry = visit_geometries[visit_id]
+            self.assertNotEqual(new_visit_record.region, old_visit_record.region)
+            self.assertEqual(new_visit_record.region, visit_geometry.visit_region)
+            for new_vdr_record in self.butler.query_dimension_records(
+                "visit_detector_region", data_id=new_visit_record.dataId
+            ):
+                self.assertEqual(
+                    new_vdr_record.region, visit_geometry.detector_regions[new_vdr_record.detector]
+                )
+        for new_exposure_record in self.butler.query_dimension_records("exposure"):
+            visit_id = new_exposure_record.id
+            visit_geometry = visit_geometries[visit_id]
+            self.assertEqual(new_exposure_record.tracking_ra, visit_geometry.boresight_ra)
+            self.assertEqual(new_exposure_record.tracking_dec, visit_geometry.boresight_dec)
+            self.assertEqual(new_exposure_record.sky_angle, visit_geometry.orientation)
+
+    def _invent_and_insert_exposure_record(self, visit_record: DimensionRecord) -> DimensionRecord:
+        group_record = self.butler.dimensions["group"].RecordClass(
+            instrument=visit_record.instrument, name=str(visit_record.id)
+        )
+        self.butler.registry.insertDimensionData("group", group_record, skip_existing=True)
+        center = LonLat(visit_record.region.getCentroid())
+        exposure_record = self.butler.dimensions["exposure"].RecordClass(
+            instrument=visit_record.instrument,
+            id=visit_record.id,
+            day_obs=visit_record.day_obs,
+            group=group_record.name,
+            physical_filter=visit_record.physical_filter,
+            tracking_ra=center.getLon().asDegrees(),
+            tracking_dec=center.getLat().asDegrees(),
+            sky_angle=float(self.rng.uniform(0.0, 5.0)),
+        )
+        self.butler.registry.insertDimensionData("exposure", exposure_record)
+        visit_definition_record = self.butler.dimensions["visit_definition"].RecordClass(
+            instrument=visit_record.instrument,
+            visit=visit_record.id,
+            exposure=exposure_record.id,
+        )
+        self.butler.registry.insertDimensionData("visit_definition", visit_definition_record)
+        return exposure_record
+
+    def _modify_polygon(self, polygon: ConvexPolygon) -> ConvexPolygon:
+        return ConvexPolygon([self._modify_point(v) for v in polygon.getVertices()])
+
+    def _modify_point(self, u: UnitVector3d) -> UnitVector3d:
+        return u.rotatedAround(self.rot_about, self.rot_amount)
 
 
 if __name__ == "__main__":
