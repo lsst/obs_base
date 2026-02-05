4f57e23c55e91e51419bb866b3ec48fe5374141c
Tim Jenness <tjenness@lsst.org>
2026-02-05 15:09:26 -0800

    Be explicit about which visit definition scheme is being tested

--- tests/test_defineVisits.py
+++ tests/test_defineVisits.py
@@ -137,7 +137,9 @@ class DefineVisitsTestCase(unittest.TestCase, DefineVisitsBase):
             shutil.rmtree(self.root, ignore_errors=True)
 
     def get_config(self) -> DefineVisitsConfig:
-        return DefineVisitsTask.ConfigClass()
+        config = DefineVisitsTask.ConfigClass()
+        config.groupExposures.name = "one-to-one-and-by-counter"
+        return config
 
     def assertVisits(self):
         """Check that the visits were registered as expected."""
